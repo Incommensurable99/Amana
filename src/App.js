349d78873aaefe2cb2d7cb31003824ecdccc5395
Lamera99 <leonardo99.ti8@gmail.com>
2025-11-28 23:29:04 -0600

    New fetch

--- src/App.js
+++ src/App.js
@@ -4,11 +4,7 @@ import Analizador from "./Analizador";
 function App() {
   return (
     <div>
-<<<<<<< HEAD
       <h1> </h1>
-=======
-      <h1>AMANA</h1>
->>>>>>> b9a0f2e01edf2eaec3f6f0b436fd5833fa2ae723
       <Analizador />
     </div>
   );
